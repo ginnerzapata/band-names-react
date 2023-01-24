837a6f5c47b7a6296a58e93ad3e558e12c40915f
Ginner Zapata <gzapata@vizzn.ca>
2023-01-24 02:49:23 -0300

    band list

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const connectSocketServer = () => {
 function App() {
   const [socket] = useState(connectSocketServer());
   const [online, setOnline] = useState(false);
+  const [bands, setBands] = useState([]);
   useEffect(() => {
     console.log({ socket });
     setOnline(socket.connected);
@@ -28,6 +29,12 @@ function App() {
   useEffect(() => {
     socket.on("disconnect", () => {
       setOnline(false);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("current-bands", (bands) => {
+      setBands(bands);
     });
   }, [socket]);
 
@@ -47,7 +54,7 @@ function App() {
       <hr />
       <div className="row">
         <div className="col-8">
-          <BandList />
+          <BandList data={bands} />
         </div>
         <div className="col-4">
           <BandAdd />
--- src/components/BandList.jsx
+++ src/components/BandList.jsx
@@ -1,23 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-export const BandList = () => {
+export const BandList = ({ data }) => {
+  const [bands, setBands] = useState(data);
+  console.log(data);
+
+  useEffect(() => {
+    setBands(data);
+  }, [data]);
+
   const createRows = () => {
-    return (
-      <tr>
+    return bands.map((b) => (
+      <tr key={b.id}>
         <td>
           <button className="btn btn-primary">+1</button>
         </td>
         <td>
-          <input type="text" className="form form-control" />
+          <input type="text" className="form form-control" value={b.name} />
         </td>
         <td>
-          <h3>15</h3>
+          <h3>{b.votes}</h3>
         </td>
         <td>
           <button className="btn btn-danger">Borrar</button>
         </td>
       </tr>
-    );
+    ));
   };
   return (
     <>
