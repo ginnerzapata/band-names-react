e4b60f65224e8bbb3cb06b9b4a83d3d5a85ea18d
Ginner Zapata <gzapata@vizzn.ca>
2023-01-24 09:30:08 -0300

    useSocket

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
 import { useState, useEffect } from "react";
 import { BandAdd } from "./components/BandAdd";
 import { BandList } from "./components/BandList";
-import io from "socket.io-client";
-
-const connectSocketServer = () => {
-  const socket = io.connect("http://localhost:8080", {
-    transports: ["websocket"],
-  });
-
-  return socket;
-};
+import { useSocket } from "./hooks/useSocket";
 
 function App() {
-  const [socket] = useState(connectSocketServer());
-  const [online, setOnline] = useState(false);
   const [bands, setBands] = useState([]);
-  useEffect(() => {
-    console.log({ socket });
-    setOnline(socket.connected);
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on("connect", () => {
-      setOnline(true);
-    });
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on("disconnect", () => {
-      setOnline(false);
-    });
-  }, [socket]);
+  const { socket, online } = useSocket("http://localhost:8080");
 
   useEffect(() => {
     socket.on("current-bands", (bands) => {
